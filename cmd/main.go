b6836db26192cd9586d4cb41f2293dd68025dfed
Eric Li <eric@artela.network>
2023-07-06 17:00:40 +0800

    test: memory-leak

--- cmd/main.go
+++ cmd/main.go
@@ -20,16 +20,18 @@ func main() {
 
 	cwd, _ := os.Getwd()
 	raw, _ := os.ReadFile(path.Join(cwd, "../wasmtime/testdata/runtime_test.wasm"))
-	for i := 0; i < 10000; i++ {
-		if i%100 == 0 {
-			fmt.Println("call times: ", i)
-			// time.Sleep(5 * time.Second)
-		}
-		callRT(raw)
-		rt.GC()
-	}
+	// for i := 0; i < 10000; i++ {
+	// 	if i%100 == 0 {
+	// 		fmt.Println("call times: ", i)
+	// 		// time.Sleep(5 * time.Second)
+	// 	}
+	// 	callRT(raw)
+	// 	rt.GC()
+	// }
+	callPoolLoop(raw)
 	// callRTLoop(raw)
 	time.Sleep(10 * time.Minute)
+	rt.GC()
 }
 
 func callPool(raw []byte) {
@@ -42,6 +44,20 @@ func callPool(raw []byte) {
 		fmt.Println("NewAspectRuntime", err)
 	}
 	pool.Return(key, wasmTimeRuntime)
+}
+
+func callPoolLoop(raw []byte) {
+	hostApis := runtime.NewHostAPIRegistry()
+	addApis(hostApis)
+
+	for i := 0; i < 10000; i++ {
+		pool := runtime.NewRuntimePool(10)
+		key, wasmTimeRuntime, err := pool.Runtime(runtime.WASM, raw, hostApis)
+		if err != nil {
+			fmt.Println("NewAspectRuntime", err)
+		}
+		pool.Return(key, wasmTimeRuntime)
+	}
 }
 
 func callRTLoop(raw []byte) {
